cd2f0a8041735f2224b4aa3a34d649173ca61639
fevilela <fernandavsac@gmail.com>
2026-02-12 22:31:02 -0300

    Safari 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -14,7 +15,12 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
         allowedHosts: true,
       },
-      plugins: [react()],
+      plugins: [
+        react(),
+        legacy({
+          targets: ['defaults', 'safari >= 12', 'ios >= 12'],
+        }),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
